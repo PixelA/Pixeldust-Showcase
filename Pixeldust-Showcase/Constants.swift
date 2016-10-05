056815da9fcc689ebde6c291b3232955ddd26740
Asherlee Levy <asherleelevy@Asherlees-MacBook-Pro.local>
2016-10-04 20:51:54 -0400

    Added constant folders and material for shadowing

---
+++ Pixeldust-Showcase/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Pixeldust-Showcase
+//
+//  Created by Asherlee Levy on 10/4/16.
+//  Copyright © 2016 Asherlee Levy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
